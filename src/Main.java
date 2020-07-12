08f572a5015a2baf3614b45f03309ecfed90c5a0
Rina043 <67234113+Rina043@users.noreply.github.com>
2020-07-12 19:37:45 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-    float balance = 2_000_000_000;
-    float transfer = 500_000_000;
-    float total = balance + transfer;
+    int balance = 2_000_000_000;
+    int transfer = 500_000_000;
+    int total = balance + transfer;
     System.out.println(total);
 }
 }
