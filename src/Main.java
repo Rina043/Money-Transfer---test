817f2f1005b086dd46b89661c3352be2ffb478da
Ekaterina Shipkova <katerinashipkova@gmail.com>
2020-07-12 19:31:25 +0400

    Report.md added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+    float balance = 2_000_000_000;
+    float transfer = 500_000_000;
+    float total = balance + transfer;
+    System.out.println(total);
+}
+}
